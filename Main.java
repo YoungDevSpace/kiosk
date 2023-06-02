f49d99d4e9d8205c8198e617b4a805e836b2d89f
YoungDevSpace <131439725+YoungDevSpace@users.noreply.github.com>
2023-06-02 12:18:07 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package task1;
+
+import task1.Kiosk;
+
+public class Main {
+    public static void main(String[] args) {
+        //Main main = new Main();
+        Kiosk kiosk = new Kiosk();
+        Kiosk.useKiosk();
+    }
+}
